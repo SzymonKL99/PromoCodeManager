b3fd1442ef636cc3b9595be1fed6656f3705ded2
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2024-05-17 12:32:19 +0200

    first commit

---
+++ src/main/java/com/example/promocodemanager/PromoCodeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.promocodemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCodeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoCodeManagerApplication.class, args);
+    }
+
+}
