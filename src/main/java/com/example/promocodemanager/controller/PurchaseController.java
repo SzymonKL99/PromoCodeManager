c223054c3183f71b3b7456ddac577eaed022a8cf
Szymon Kliszcz <szymon.kliszczz@gmail.com>
2024-05-21 21:10:51 +0200

    added endpoints in PurchaseController

--- src/main/java/com/example/promocodemanager/controller/PurchaseController.java
+++ src/main/java/com/example/promocodemanager/controller/PurchaseController.java
@@ -1,11 +1,38 @@
 package com.example.promocodemanager.controller;
 
+import com.example.promocodemanager.dto.PurchaseDto;
+import com.example.promocodemanager.exceptions.ProductNotFoundExceptions;
+import com.example.promocodemanager.exceptions.PromoCodeNotFoundException;
+import com.example.promocodemanager.service.PurchaseServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/purchase")
 public class PurchaseController {
+
+    private final PurchaseServiceImpl purchaseService;
+
+    @GetMapping(value = "/discountedPrice")
+    public ResponseEntity<BigDecimal> getDiscountedPrice(
+            @RequestParam("productId") Long productId,
+            @RequestParam("promoCodeId") Long promoCodeId) {
+
+            BigDecimal discountedPrice = purchaseService.calculateDiscountedPrice(productId, promoCodeId);
+            return ResponseEntity.ok(discountedPrice);
+    }
+
+    @PostMapping(value = "/simulate")
+    public ResponseEntity<PurchaseDto> simulatePurchase(
+            @RequestParam("productId") Long productId,
+            @RequestParam("promoCodeId") Long promoCodeId) {
+
+            PurchaseDto purchaseDto = purchaseService.simulatePurchase(productId, promoCodeId);
+            return ResponseEntity.ok(purchaseDto);
+    }
 }
